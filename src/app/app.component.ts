e072a720a1d91791d738b9a9642a5113b01e739e
jdjuan <david.juanherrera@gmail.com>
2018-08-02 18:45:38 -0400

    feat: fetch api for yes or no

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  data$: Observable<any>;
+
+  constructor(private http: HttpClient) {
+    this.data$ = http.get('https://yesno.wtf/api');
+  }
 }
